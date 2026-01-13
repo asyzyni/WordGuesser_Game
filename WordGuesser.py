0c8666f8ebb2fafea3ccfa9af730e5933ba9b898
Asyifa Izayani <156439863+asyzyni@users.noreply.github.com>
2026-01-13 17:25:53 +0800

    finish the algorythm

---
+++ WordGuesser.py
@@ -0,0 +1,36 @@
+import random 
+
+
+# import the list of words 
+word_bank = ["python", "java", "kotlin", "javascript", "hangman", "programming", "developer", "function", "variable", "loop"]
+
+# make the random word selection function
+word = random.choice(word_bank)
+
+# initialize the guessed letters list and the number of attempts
+guess_word = ["_"] * len(word)
+attempts = 5 
+
+# Game loop 
+
+while attempts > 0:
+    print("\nCurrent word: " + " ".join(guess_word))
+    guess = input("Tebak deh :").lower()
+    
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess: 
+                guess_word[i] = guess
+        print("Benar!")
+    else:
+        attempts -= 1 
+        print(f"Salah! Kesempatan tersisa: {attempts}")
+        
+    if "_" not in guess_word:
+        print("\nSelamat! Kamu menebak kata: " + word)
+        break 
+
+if attempts == 0 and "_" in guess_word:
+    print("\nMaaf, kesempatanmu habis. Kata yang benar adalah: " + word)
+            
+    
